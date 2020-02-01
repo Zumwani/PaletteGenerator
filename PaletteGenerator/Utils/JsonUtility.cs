0241777d8cc8927179d964cbcca466a507f338b1
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2020-02-01 23:14:36 +0100

    Made json serializer convert colors to and from hex.

--- PaletteGenerator/Utils/JsonUtility.cs
+++ PaletteGenerator/Utils/JsonUtility.cs
@@ -4,12 +4,22 @@ using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace PaletteGenerator
 {
 
     public static class JsonUtility<T> where T : new() 
     {
+
+        static JsonUtility()
+        {
+            options = new JsonSerializerOptions();
+            options.Converters.Add(new ConvertColorToHex());
+            options.WriteIndented = true;
+        }
+
+        static readonly JsonSerializerOptions options;
 
         public static async Task<T> PromptAndLoad() =>  await Load(PromptUtility.Load() ?? string.Empty);
         public static void PromptAndSave(T obj)     => Save(obj, PromptUtility.Save() ?? string.Empty);
@@ -28,7 +38,7 @@ namespace PaletteGenerator
 
             try
             {
-                return JsonSerializer.Deserialize<T>(await File.ReadAllTextAsync(path));
+                return JsonSerializer.Deserialize<T>(await File.ReadAllTextAsync(path), options);
             }
             catch (Exception e)
             {
@@ -48,7 +58,7 @@ namespace PaletteGenerator
 
                 try
                 {
-                    File.WriteAllText(path, JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true }));
+                    File.WriteAllText(path, JsonSerializer.Serialize(obj, options));
                 }
                 catch (Exception e)
                 {
@@ -59,6 +69,17 @@ namespace PaletteGenerator
 
         static void ErrorMessage(Exception e, string action) =>
             MessageBox.Show(e.GetType().Name + ":" + Environment.NewLine + Environment.NewLine + e.Message + Environment.NewLine + Environment.NewLine, $"An error occured while {action} json file.");
+
+        public class ConvertColorToHex : System.Text.Json.Serialization.JsonConverter<Color>
+        {
+
+            public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+                (Color)ColorConverter.ConvertFromString(reader.GetString());
+
+            public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options) =>
+                writer.WriteStringValue((new ColorConverter()).ConvertToString(value));
+
+        }
 
     }
 
